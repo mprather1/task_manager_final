ad9e6634578dc869925da44960fd63cb2538b107
Michael Prather <mprather1@gmail.com>
2016-12-13 04:30:05 +0000

    update

--- app/views/TaskView.js
+++ app/views/TaskView.js
@@ -1,11 +1,23 @@
 var TaskView = Backbone.Marionette.View.extend({
+  
   tagName: 'tr',
+  
   className: 'table-row',
+  
   template: require("../templates/task-view-template.html"),
+  
   attributes: function(){
     return {
     "data-id": this.model.get('id'),
     };
+  },
+  
+  id: function(){
+    if (this.options.model.attributes.completed == true){
+      return 'completed';
+    } else {
+      return 'active';
+    }
   },
 });
 
