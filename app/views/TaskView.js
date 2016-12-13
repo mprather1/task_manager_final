a0e7d394d96763754bec9128ca6162dae91f7685
Michael Prather <mprather1@gmail.com>
2016-12-13 04:09:10 +0000

    removed console.log

--- app/views/TaskView.js
+++ app/views/TaskView.js
@@ -2,9 +2,6 @@ var TaskView = Backbone.Marionette.View.extend({
   tagName: 'tr',
   className: 'table-row',
   template: require("../templates/task-view-template.html"),
-  initialize: function(){
-    console.log(this)
-  },
   attributes: function(){
     return {
     "data-id": this.model.get('id'),
